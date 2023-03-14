8f4bba59cdb98257b50239cf7dfe76244413df82
NomanGhayyur <nomanghayyur96@gmail.com>
2023-03-14 07:28:46 +0500

    frontend backend integration

--- server/index.js
+++ server/index.js
@@ -1,17 +1,46 @@
 const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
+const bodyPrser = require("body-parser");
+const cors = require("cors");
+
 const configuration = new Configuration({
   organization: "org-STW5TeQDB7SbMdlzFotmk3v2",
-  apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-async function callApi() {
+//creating a simple api that calls the function above
+
+const app = express();
+app.use(bodyPrser.json());
+app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+const port = 3080;
+
+app.post("/", async (req, res) => {
+  const { message, currentModel } = req.body;
+  // console.log(message, "message");
   const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
+    model: `${currentModel}`,
+    // model: "text-davinci-003",
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
   });
-  console.log(response.data.choices[0].text);
-}
-callApi();
+  res.json({
+    message: response.data.choices[0].text,
+  });
+});
+
+app.get("/models", async (req, res) => {
+  const response = await openai.listEngines();
+  console.log(response.data.data);
+  res.json({
+    models: response.data.data,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listning at http://localhost:${port}`);
+});
